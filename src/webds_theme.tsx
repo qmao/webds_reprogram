27cabfde302f24daf234dfcaad14c55eba5dc4e5
root <root@pi-syna>
2022-03-22 00:06:01 -0700

    add theme

--- src/webds_theme.tsx
+++ src/webds_theme.tsx
@@ -1,1 +1,42 @@
-../../common/theme/webds_theme.tsx
+import { createTheme } from "@mui/material/styles";
+
+const webdsTheme = createTheme({
+  palette: {
+    primary: {
+      light: '#5aacf6',
+      main: '#007dc3',
+      dark: '#005192',
+      contrastText: '#fff'
+    }
+  },
+
+  typography: {
+    fontFamily: [
+      'Arial',
+      'Roboto',
+      'Helvetica',
+      'sans-serif'
+    ].join(','),
+  },
+
+  components: {
+    MuiAvatar: {
+      defaultProps: {
+        sx: {bgcolor: '#007dc3'}
+      }
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained'
+      }
+    },
+    MuiFab: {
+      defaultProps: {
+        color: 'primary',
+        size: 'small'
+      }
+    }
+  },
+});
+
+export default webdsTheme
